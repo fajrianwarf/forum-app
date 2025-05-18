436d7147f5cccba9968018a817350c23591c4f99
Fajri Anwar Fachrul <f.fachrul@teravintech.com>
2025-05-18 12:06:17 +0700

    - Modify : make test pass

--- src/slices/threadsSlice.test.js
+++ src/slices/threadsSlice.test.js
@@ -52,7 +52,7 @@ describe('threadsReducer - reducer', () => {
 
       const nextState = threadsReducer(state, resetData('statusList'));
 
-      expect(nextState.statusList).toBe(initialState.threadList);
+      expect(nextState.statusList).toBe(initialState.statusList);
     });
 
     it('should reset multiple fields to initialState', () => {
